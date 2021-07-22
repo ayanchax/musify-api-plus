a3ce11fb4324dcaa6d2ca36e7a86e756eb82dada
Swaroop <swaroop.chakraborty@cerner.com>
2021-07-22 01:00:47 -0400

    commit

--- routes/route.js
+++ routes/route.js
@@ -410,6 +410,7 @@ router.get("/playlist", (req, res, next) => {
             });
         })
         .catch((err) => {
+            console.log(err);
             res.status(500).json({
                 msg: messages.ERROR,
                 diagnostics: err,
